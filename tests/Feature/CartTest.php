a7d5388a702005d1e0431d014716a159ca6ec1c3
Gonçalo <goncalo.graca2000@gmail.com>
2023-01-15 18:21:11 +0000

    modified:   .github/workflows/build.yml 	modified:   tests/Feature/CartTest.php 	modified:   tests/Feature/ProductTest.php 	modified:   tests/Feature/UserTest.php

--- tests/Feature/CartTest.php
+++ tests/Feature/CartTest.php
@@ -22,4 +22,93 @@ final class CartTest extends TestCase
 
     }
 
+    public function testAddProductToCart()
+    {
+
+        $user = User::factory()->create();
+
+        $product = Product::factory()->create();
+
+        $response = $this->actingAs($user)->post('/cart/addProduct/'.$product->id, [
+            'quantity' => 1,
+        ]);
+
+        $response->assertStatus(302);
+
+        $this->assertDatabaseHas('carts', [
+            'user_id' => $user->id,
+            'product_id' => $product->id,
+            'quantity' => 1,
+            'price' => $product->price,
+        ]);
+
+    }
+
+    public function testRemoveProductFromCart()
+    {
+
+        $user = User::factory()->create();
+
+        $product = Product::factory()->create();
+
+        $cart = Cart::factory()->create([
+            'user_id' => $user->id,
+            'product_id' => $product->id,
+            'quantity' => 1,
+            'price' => $product->price,
+        ]);
+
+        $response = $this->actingAs($user)->post('/cart/removeProduct/'.$product->id);
+
+        $response->assertStatus(302);
+
+        $this->assertDatabaseMissing('carts', [
+            'user_id' => $user->id,
+            'product_id' => $product->id,
+            'quantity' => 1,
+            'price' => $product->price,
+        ]);
+
+    }
+
+    public function testAlterProductQuantityCart()
+    {
+
+        $user = User::factory()->create();
+
+        $product = Product::factory()->create();
+
+        $cart = Cart::factory()->create([
+            'user_id' => $user->id,
+            'product_id' => $product->id,
+            'quantity' => 1,
+            'price' => $product->price,
+        ]);
+
+        $response = $this->actingAs($user)->post('/cart/productQuantity/'.$product->id, [
+            'quantity' => 2,
+        ]);
+
+        $response->assertStatus(302);
+
+        $this->assertDatabaseHas('carts', [
+            'user_id' => $user->id,
+            'product_id' => $product->id,
+            'quantity' => 2,
+            'price' => $product->price * 2,
+        ]);
+
+    }
+
+    public function testCartCheckout()
+    {
+
+        $user = User::factory()->create();
+
+        $response = $this->actingAs($user)->post('/cart/checkout');
+
+        $response->assertStatus(200);
+
+    }
+
 }
